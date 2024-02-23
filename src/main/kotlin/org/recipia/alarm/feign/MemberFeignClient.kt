e8e04deae87b640d1cd1c7d3a719298a2251e913
yijoon009 <yijoon009@gmail.com>
2024-02-23 23:54:36 +0900

    [RCP-403] :sparkles: 멤버 서버로 feign 요청 날리는 client 생성

---
+++ src/main/kotlin/org/recipia/alarm/feign/MemberFeignClient.kt
@@ -0,0 +1,18 @@
+package org.recipia.alarm.feign
+
+import org.recipia.alarm.dto.NicknameDto
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "member-service", url = "\${feign.member_url}")
+interface MemberFeignClient {
+
+    /**
+     * 멤버 서버로부터 Id로 해당하는 Nickname 값을 받아온다.
+     */
+    @RequestMapping(method = [RequestMethod.POST], value = ["/feign/member/getNickname"])
+    fun getNickname(@RequestParam("memberId") memberId: Long): NicknameDto
+
+}
