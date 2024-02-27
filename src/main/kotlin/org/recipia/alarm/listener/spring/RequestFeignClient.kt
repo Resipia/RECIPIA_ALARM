8e07e63b466756011539f191829cd3de37a61a9b
yijoon009 <yijoon009@gmail.com>
2024-02-27 12:42:14 +0900

    [RCP-413] :sparkles: dynamodb 저장을 스프링 이벤트로 관리

--- src/main/kotlin/org/recipia/alarm/listener/spring/RequestFeignClient.kt
+++ src/main/kotlin/org/recipia/alarm/listener/spring/RequestFeignClient.kt
@@ -1,36 +1,30 @@
 package org.recipia.alarm.listener.spring
 
-import jakarta.transaction.Transactional
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.recipia.alarm.common.springevent.MemberSignupSpringEvent
 import org.recipia.alarm.dto.NicknameDto
 import org.recipia.alarm.feign.MemberFeignClient
-import org.recipia.alarm.service.DynamoDBService
+import org.recipia.alarm.springevent.FeignNicknameSpringEvent
+import org.recipia.alarm.springevent.MemberSignupSpringEvent
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class RequestFeignClient (
-    val memberFeignClient: MemberFeignClient,
-    val dynamoDBService: DynamoDBService
-
+        private val memberFeignClient: MemberFeignClient,
+        private val eventPublisher: ApplicationEventPublisher
 ) {
 
     /**
      * Feign 클라이언트로 Member서버에서 회원가입되어 저장된 닉네임을 요청하는 리스너
      */
-    @Transactional
     @EventListener
     fun requestSignUpMemberNickname(event: MemberSignupSpringEvent)  {
         val memberId = event.memberId
 
         val nicknameDto: NicknameDto = memberFeignClient.getNickname(memberId)
 
-        // DynamoDBService를 사용하여 DynamoDB에 데이터 추가
-        GlobalScope.launch {
-            dynamoDBService.addNicknameToDB(nicknameDto.memberId, nicknameDto.nickname)
-        }
-
+        // member 서버에서 응답받은 NicknameDto가 존재할때 DynamoDB 저장을 위한 스프링 이벤트 발행
+        val feignNicknameSpringEvent: FeignNicknameSpringEvent = FeignNicknameSpringEvent(nicknameDto)
+        eventPublisher.publishEvent(feignNicknameSpringEvent)
     }
 }
